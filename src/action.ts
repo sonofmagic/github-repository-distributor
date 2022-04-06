1a5338695f6f145425af1b74bc3fde177149804a
SonOfMagic <1324318532@qq.com>
2022-04-06 21:26:22 +0800

    feat: commit getBooleanInput

--- src/action.ts
+++ src/action.ts
@@ -1,18 +1,18 @@
 import type { UserDefinedOptions } from './type'
 import core from '@actions/core'
 import github from '@actions/github'
-import { parseBoolean } from './util'
+
 // export { default as core } from '@actions/core'
 // export { default as github } from '@actions/github'
 export function getActionOptions (): UserDefinedOptions {
   const token = core.getInput('token')
   const username = core.getInput('username')
-  const motto = parseBoolean(core.getInput('motto'))
+  const motto = core.getBooleanInput('motto')
   const filepath = core.getInput('filepath')
   const title = core.getInput('title')
-  const includeFork = parseBoolean(core.getInput('includeFork'))
-  const includeArchived = parseBoolean(core.getInput('includeArchived'))
-  const onlyPrivate = parseBoolean(core.getInput('onlyPrivate'))
+  const includeFork = core.getBooleanInput('includeFork')
+  const includeArchived = core.getBooleanInput('includeArchived')
+  const onlyPrivate = core.getBooleanInput('onlyPrivate')
   return {
     token,
     username,
